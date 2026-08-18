4b4c2d6d8dcda908e36275d9c0e4497ea0611022
Adrien Wagner <ernstmeier51@gmail.com>
2026-08-18 18:18:54 +0200

    refactor:helper functions for click operations

--- handler.js
+++ handler.js
@@ -1,31 +1,37 @@
-class AutoClicker {
-    constructor(interval) {
-        this.interval = interval;
-        this.clicking = false;
-        this.clickId = null;
-    }
-
-    start() {
-        if (this.clicking) return;
-        this.clicking = true;
-        this.clickId = setInterval(() => this.triggerClick(), this.interval);
-    }
-
-    stop() {
-        if (!this.clicking) return;
-        clearInterval(this.clickId);
-        this.clicking = false;
-        this.clickId = null;
-    }
-
-    triggerClick() {
-        const event = new MouseEvent('click', { bubbles: true });
-        document.dispatchEvent(event);
+function clickElement(selector) {
+    const element = document.querySelector(selector);
+    if (element) {
+        element.click();
+    } else {
+        console.error(`Element not found: ${selector}`);
     }
 }
 
-const clicker = new AutoClicker(1000);
-clicker.start();
+function scheduleClick(selector, delay) {
+    setTimeout(() => clickElement(selector), delay);
+}
 
-// Stop clicking after 10 seconds
-setTimeout(() => clicker.stop(), 10000);
+function startAutoClicker(selector, interval) {
+    const intervalId = setInterval(() => clickElement(selector), interval);
+    return intervalId;
+}
+
+function stopAutoClicker(intervalId) {
+    clearInterval(intervalId);
+}
+
+function toggleAutoClicker(selector, interval) {
+    let isActive = false;
+    let intervalId;
+    return function() {
+        if (!isActive) {
+            isActive = true;
+            intervalId = startAutoClicker(selector, interval);
+        } else {
+            stopAutoClicker(intervalId);
+            isActive = false;
+        }
+    };
+}
+
+export { clickElement, scheduleClick, startAutoClicker, stopAutoClicker, toggleAutoClicker };
